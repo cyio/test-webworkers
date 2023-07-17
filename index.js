9f06c41fb8d02111a860bee6ef42174a1637ec58
gongyi01 <gongyi01@baidu.com>
2023-07-17 10:25:38 +0800

    canvas offscreen

--- index.js
+++ index.js
@@ -3,6 +3,16 @@
 let worker = new Worker('worker.js')
 const handleMessage = (event) => {
     console.log(event.data)
+    const { method, args } = event.data
+    if (method === 'process-image') {
+        // 从消息中获取渲染好的图像数据
+        const { imageData } = args;
+
+        // 将图像数据绘制到 Canvas 上
+        const canvas = document.getElementById("myCanvas");
+        const ctx = canvas.getContext("2d");
+        ctx.putImageData(imageData, 0, 0);
+    }
 }
 
 worker.addEventListener('message', handleMessage);
@@ -29,4 +39,10 @@ worker.postMessage({
     args: {
         arr: arr1
     }
+})
+
+worker.postMessage({
+  method: 'process-image',
+  args: {
+  }
 })
--- worker.js
+++ worker.js
@@ -19,7 +19,46 @@ self.addEventListener('message', function ({data}) {
         }
     
         main();
+    } else if (method === 'process-image') {   
+        const imageData = getImageDataByOffscreenCanvas()
+        // 将图像数据发送回主线程
+        self.postMessage({
+            method: 'process-image',
+            args: {
+                imageData
+            }
+        });
     }
 
     // self.postMessage('You said: ' + e.data);
   }, false);
+
+
+function getImageData() {
+// 创建一个新的 Uint8ClampedArray 对象
+    const imageData = new Uint8ClampedArray(400 * 400 * 4);
+
+    // 进行图像处理操作，例如将整个图像设置为红色
+    for (let i = 0; i < imageData.length; i += 4) {
+        imageData[i] = 255;   // 红色通道
+        imageData[i + 1] = 0; // 绿色通道
+        imageData[i + 2] = 0; // 蓝色通道
+        imageData[i + 3] = 255; // 透明度通道
+    }
+
+    // 创建一个新的 ImageData 对象
+    return new ImageData(imageData, 400, 400);
+}
+
+function getImageDataByOffscreenCanvas() {
+    // 创建一个新的 Canvas 对象
+    const canvas = new OffscreenCanvas(400, 400);
+    const ctx = canvas.getContext('2d');
+
+    // 在 Canvas 上进行绘制
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(0, 0, 400, 400);
+
+    // 获取绘制好的图像数据
+    return ctx.getImageData(0, 0, 400, 400);
+}
